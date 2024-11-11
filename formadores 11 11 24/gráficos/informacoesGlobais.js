dd56205b29819427c11fc906276bfcad8a2ae191
Deciop <125397915+Deciop@users.noreply.github.com>
2024-11-11 20:56:58 -0300

    11 11 24

---
+++ formadores 11 11 24/gráficos/informacoesGlobais.js
@@ -0,0 +1,17 @@
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json';
+
+async function vizualizarinformacoesGlobais() {
+    const res = await fetch(url);
+    const dados = await res.json();
+   
+    
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `O mundo tem<span>${dados.total_pessoas_mundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${dadostotal_trabalhadores_mundo}</span>   bilhões estão empregadas e passam em média <span>${dados.tempo_medio_deslocamento_para_trabalho
+}</span>  horas por dia trabalhando.
+Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais. `
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo);
+}
+
+vizualizarinformacoesGlobais();
