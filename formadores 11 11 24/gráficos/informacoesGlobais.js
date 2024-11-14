7989d469b8a4e5ebb6437527f4440e1b5cd066f5
Deciop <125397915+Deciop@users.noreply.github.com>
2024-11-13 22:48:51 -0300

    Update informacoesGlobais.js

--- formadores 11 11 24/gráficos/informacoesGlobais.js
+++ formadores 11 11 24/gráficos/informacoesGlobais.js
@@ -1,17 +1,17 @@
-const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json';
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function vizualizarinformacoesGlobais() {
-    const res = await fetch(url);
-    const dados = await res.json();
-   
-    
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `O mundo tem<span>${dados.total_pessoas_mundo}</span>  bilhões de pessoas, dessas pessoas, aproximadamente <span>${dadostotal_trabalhadores_mundo}</span>   bilhões estão empregadas e passam em média <span>${dados.tempo_medio_deslocamento_para_trabalho
-}</span>  horas por dia trabalhando.
-Temos, portanto, mais da metade da população mundial que não exerce trabalhos com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. Porém, também pode significar muitos postos de trabalho chamados informais. `
+    
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+    console.log(paragrafo)
+
     const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo);
+    container.appendChild(paragrafo)
 }
 
-vizualizarinformacoesGlobais();
+vizualizarInformacoesGlobais()
